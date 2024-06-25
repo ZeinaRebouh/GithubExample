bf49bf5dbb4574cdb9e86901468303df0b4c1f72
ZeinaRebouh <missionstreetcat@gmail.com>
2024-06-25 09:44:39 -0700

    Initial Commit

---
+++ GithubExample/GithubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExampleApp.swift
+//  GithubExample
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
